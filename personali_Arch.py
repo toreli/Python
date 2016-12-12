2e7c8a7d7cb2787cf585332ab990cf9f412eaa01
toreli <davidjrtoreli@gmail.com>
2016-12-11 23:51:45 -0200

    Add files via upload

---
+++ personali_Arch.py
@@ -0,0 +1,165 @@
+#!/usr/bin/python
+# coding: utf-8 -*-
+from os import system
+'''
+Personalizador de temas para Arch*Linux
+'''
+
+def banner():
+	system('clear')
+	print ("""\033[01;32m
+
+ \t\t\t██▓ ███▄    █   ██████ ▄▄▄█████▓ ▄▄▄       ██▓     ██▓    
+\t\t\t▓██▒ ██ ▀█   █ ▒██    ▒ ▓  ██▒ ▓▒▒████▄    ▓██▒    ▓██▒    
+\t\t\t▒██▒▓██  ▀█ ██▒░ ▓██▄   ▒ ▓██░ ▒░▒██  ▀█▄  ▒██░    ▒██░    
+\t\t\t░██░▓██▒  ▐▌██▒  ▒   ██▒░ ▓██▓ ░ ░██▄▄▄▄██ ▒██░    ▒██░    
+\t\t\t░██░▒██░   ▓██░▒██████▒▒  ▒██▒ ░  ▓█   ▓██▒░██████▒░██████▒
+\t\t\t░▓  ░ ▒░   ▒ ▒ ▒ ▒▓▒ ▒ ░  ▒ ░░    ▒▒   ▓▒█░░ ▒░▓  ░░ ▒░▓  ░
+\t\t\t ▒ ░░ ░░   ░ ▒░░ ░▒  ░ ░    ░      ▒   ▒▒ ░░ ░ ▒  ░░ ░ ▒  ░
+\t\t\t ▒ ░   ░   ░ ░ ░  ░  ░    ░        ░   ▒     ░ ░     ░ ░   
+\t\t\t ░           ░       ░                 ░  ░    ░  ░    ░  ░
+                                                      
+\t\t\t   █████▓▓██   ███  ███▄ ▄███▓ ▄▄▄        ██████           
+\t\t\t       ██▒ ▓▒▓█   ▀ ▓██▒▀█▀ ██▒▒████▄    ▒██    ▒           
+\t\t\t    ▒ ▓██░ ▒░▒███   ▓██    ▓██░▒██  ▀█▄  ░ ▓██▄             
+\t\t\t    ░ ▓██▓ ░ ▒▓█  ▄ ▒██    ▒██ ░██▄▄▄▄██   ▒   ██▒          
+\t\t\t      ▒██▒ ░ ░▒████▒▒██▒   ░██▒ ▓█   ▓██▒▒██████▒▒          
+\t\t\t     ▒ ░░   ░░ ▒░ ░░ ▒░   ░  ░ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░          
+\t\t\t       ░     ░ ░  ░░  ░      ░  ▒   ▒▒ ░░ ░▒  ░ ░          
+\t\t\t     ░         ░   ░      ░     ░   ▒   ░  ░  ░            
+\t\t\t     ░  ░       ░         ░  ░      ░            
+\033[01;37m""")
+#6
+def deepen():
+	system('clear')
+	print("""\033[01;33m
+\t\t\t\t████████▄     ▄████████    ▄████████    ▄███████▄  ▄█  ███▄▄▄▄   
+\t\t\t\t███   ▀███   ███    ███   ███    ███   ███    ███ ███  ███▀▀▀██▄ 
+\t\t\t\t███    ███   ███    █▀    ███    █▀    ███    ███ ███▌ ███   ███ 
+\t\t\t\t███    ███  ▄███▄▄▄      ▄███▄▄▄       ███    ███ ███▌ ███   ███ 
+\t\t\t\t███    ███ ▀▀███▀▀▀     ▀▀███▀▀▀     ▀█████████▀  ███▌ ███   ███ 
+\t\t\t\t███    ███   ███    █▄    ███    █▄    ███        ███  ███   ███ 
+\t\t\t\t███   ▄███   ███    ███   ███    ███   ███        ███  ███   ███ 
+\t\t\t\t████████▀    ██████████   ██████████  ▄████▀      █▀    ▀█   █▀  
+""")
+	system('sudo pacman -S deepin deepin-extra')
+#5
+def numixCircle():
+	system('clear')
+	print("""\033[01;31
+\t\t\t\tooooo      ooo ooooo     ooo ooo        ooooo ooooo ooooooo  ooooo     
+\t\t\t\t`888b.     `8' `888'     `8' `88.       .888' `888'  `8888    d8'      
+\t\t\t\t 8 `88b.    8   888       8   888b     d'888   888     Y888..8P        
+\t\t\t\t 8   `88b.  8   888       8   8 Y88. .P  888   888      `8888'         
+\t\t\t\t 8     `88b.8   888       8   8  `888'   888   888     .8PY888.        
+\t\t\t\t 8       `888   `88.    .8'   8    Y     888   888    d8'  `888b       
+\t\t\t\to8o        `8     `YbodP'    o8o        o888o o888o o888o  o88888o     
+\t\t\t\t  .oooooo.   ooooo ooooooooo.     .oooooo.   ooooo        oooooooooooo 
+\t\t\t\t d8P'  `Y8b  `888' `888   `Y88.  d8P'  `Y8b  `888'        `888'     `8 
+\t\t\t\t888           888   888   .d88' 888           888          888         
+\t\t\t\t888           888   888ooo88P'  888           888          888oooo8    
+\t\t\t\t888           888   888`88b.    888           888          888    "    
+\t\t\t\t`88b    ooo   888   888  `88b.  `88b    ooo   888       o  888       o 
+\t\t\t\t `Y8bood8P'  o888o o888o  o888o  `Y8bood8P'  o888ooooood8 o888ooooood8 
+""")
+	system('yaourt numix-circle')
+
+#1
+def install_gnome():
+	system('clear')
+	print("""\033[01;36m
+\t\t\t\t\t▄▀     ▄   ████▄ █▀▄▀█ ▄███▄   
+\t\t\t\t\t▄▀        █  █   █ █ █ █ █▀   ▀  
+\t\t\t\t\t█ ▀▄  ██   █ █   █ █ ▄ █ ██▄▄    
+\t\t\t\t\t█   █ █ █  █ ▀████ █   █ █▄   ▄▀ 
+\t\t\t\t\t ███  █  █ █          █  ▀███▀   
+\t\t\t\t\t      █   ██         ▀           
+                                 
+""")
+	system('sudo pacman -S gnome gnome-extra gdm')
+	print ("\t\t\t\t INICIANDO O GDM PARA GNOME PRESSION ENTER PARA PROSSEGUIR")
+	input()
+	system('sudo systemctl enable gdm.services')
+	system('sleep 2')
+	print("\t\t\t\t INSTALANDO O GNOME TWEAK-TOOL")
+	system('sleep 1')
+	system('sudo pacman -S gnome-tweak-tool')
+	system('sleep 2')
+	print ("\t\t\t\t INSTALANDO GNOME SOFTWARE")
+	system('yaourt gnome-software')
+#2
+def install_xfce4():
+	system('clear')
+	print ("""
+\t\t\t\t\033[01;31m██╗  ██╗███████╗ ██████╗███████╗██╗  ██╗
+\t\t\t\t╚██╗██╔╝██╔════╝██╔════╝██╔════╝██║  ██║
+\t\t\t\t ╚███╔╝ █████╗  ██║     █████╗  ███████║
+\t\t\t\t ██╔██╗ ██╔══╝  ██║     ██╔══╝  ╚════██║
+\t\t\t\t██╔╝ ██╗██║     ╚██████╗███████╗     ██║
+\t\t\t\t╚═╝  ╚═╝╚═╝      ╚═════╝╚══════╝     ╚═╝
+                                        
+""")
+	system('sudo pacman -S xfce4 xfce4-goodeis')
+	print ("\033[01;32m\t\t\t\t INSTALANDO GERENCIADOR DE INSTERFACE GRAFICA LIGHTDM")
+	system('sudo pacman -S lightdm')
+	print ("\033[01;32m\t\t\t\t INSTALANDO GERENCIADOR DE TEMAS")
+	system('yaourt xfce-theme-manager')
+#3	
+def install_kde():
+	system('clear')
+	print("""\033[01;34m
+\t\t\t\t /$$   /$$ /$$$$$$$  /$$$$$$$$
+\t\t\t\t| $$  /$$/| $$__  $$| $$_____/
+\t\t\t\t| $$ /$$/ | $$  \ $$| $$      
+\t\t\t\t| $$$$$/  | $$  | $$| $$$$$   
+\t\t\t\t| $$  $$  | $$  | $$| $$__/   
+\t\t\t\t| $$\  $$ | $$  | $$| $$      
+\t\t\t\t| $$ \  $$| $$$$$$$/| $$$$$$$$
+\t\t\t\t|__/  \__/|_______/ |________/
+                            \033[01;37m""")
+	system('sudo pacman -S plasma kdebase sddm')
+	print ("\033[01;32m\t\t\t\tINSTALANDO GERENCIADOR DE INSTERFACE SDDM")
+	system("sudo systemctl enable sddm")
+	input()
+
+def installMain():
+	try:
+		banner()
+		print ("\t\t\t\t*************************************************")
+		print ("\t\t\t\t* \tE QUE COMECEM AS PERSONALIZAÇÕES\t*")
+		print ("\t\t\t\t* \t\033[01;31m@utor:davidToreli \t\t\t\033[01;37m*")
+		print ("\t\t\t\t* \t\033[01;31m@amail:davidjtoreli@gmail.com\t\t\033[01;37m*")
+		print ("\t\t\t\t* ---------------------------------------------\t*")
+		print ("\t\t\t\t* [1]-> INSTALAR GNOME\t\t\t\t*")
+		print ("\t\t\t\t* [2]-> INSTALAR XFCE4\t\t\t\t*")
+		print ("\t\t\t\t* [3]-> INSTALAR KDE\t\t\t\t*")
+		print ("\t\t\t\t* [4]-> INSTALAR NUMIX CIRCLE\t\t\t*")
+		print ("\t\t\t\t* [5]-> INSTALAR DEEPIN DEEPIN-EXTRA \t\t*")
+		print ("\t\t\t\t*************************************************")
+		opcoes = int(input("\t\t\t\tE que comecem as Escolhas ;).: "))
+
+		if opcoes < 1 or opcoes > 5:
+			print("\n\n\t\t\t\t\t OPÇÃO INVALIDA")
+			system('sleep 0.50')
+			installMain()
+		elif opcoes == 1:
+			install_gnome()
+			installMain()
+		elif opcoes ==2:
+			install_xfce4()
+			installMain()
+		elif opcoes ==3:
+			install_kde()
+			installMain()
+		elif opcoes ==4:
+			print ("\t\t\t\t INSTALANDO ICONES NUMIX CIRCLE")
+			numixCircle()
+			installMain()
+		else:
+			print ("\t\t\t\t INSTALANDO ICONES E TEMAS DEEPIN")
+			deepen()
+			installMain()
+	except KeyboardInterrupt:
+		print("\n\n\t\t\t\t\t CTRL+C PRESSIONADO")
+
+installMain()
